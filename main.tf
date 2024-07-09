afac52305c98af49d743aa0b79e3f36c8ad97b73
goku31s <sukrit.ss.31@gmail.com>
2024-07-09 15:06:20 +0000

    Created GKE creation roles

--- main.tf
+++ main.tf
@@ -1,19 +1,15 @@
-resource "google_container_cluster" "primary" {
-  name     = "my-gke-cluster"
+resource "google_container_cluster" "mygke" {
+  name = "my-gke-cluster"
   location = "us-east1"
-
-  remove_default_node_pool = true
-  initial_node_count       = 1
-}
-
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "my-node-pool"
-  cluster    = google_container_cluster.primary.name
-  location   = "us-east1"
-  node_count = 3
-
-  node_config {
-    machine_type = "e2-medium"
-    disk_size_gb = 20
+  network_policy {
+    enabled = true
+  }
+  node_pool {
+    name = "default-pool"
+    node_count = 1
+    node_config {
+      machine_type = "e2-medium"
+      disk_size_gb = 20
+    }
   }
 }
